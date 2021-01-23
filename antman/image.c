5cd8f950e5a86d3aefa7de531b8d5eb5a3d569b4
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2021-01-23 14:31:26 +0100

    optimise image compression and remove malloc

--- antman/image.c
+++ antman/image.c
@@ -7,73 +7,40 @@
 
 #include <stdlib.h>
 #include <unistd.h>
-#include "my.h"
-
-int my_getnbr(char const *str)
-{
-    int i = 0;
-    int neg = 1;
-    long number = 0;
-
-    while (str[i] != '\0' && (str[i] == '-' || str[i] == '+')) {
-        if (str[i] == '-')
-            neg *= -1;
-        i++;
-    }
-    while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9')) {
-        number = number * 10 + (str[i] - '0');
-        i++;
-    }
-    number *= neg;
-    if (number > 2147483647 || number < -2147483648)
-        return (0);
-    return (number);
-}
-
-void my_strncpy_pos(char *dest, char const *src, int n, int pos)
-{
-    unsigned char add_border = 1;
-    int i = 0;
-
-    while (i < n) {
-        dest[i] = src[i + pos];
-        if (src[i + pos] == '\0') {
-            add_border = 1;
-            break;
-        }
-        i++;
-    }
-    if (add_border)
-        dest[i] = '\0';
-}
 
 static void skip_header(char *str, int *i)
 {
+    int header_len = 0;
+
     for (int n = 0 ; n != 5; *i += 1) {
         if (str[*i] == '\n')
             n++;
-        my_putchar(str[*i]);
+        header_len++;
     }
+    write(1, str, header_len);
+}
+
+static void print_color(char *str, int pos, int nb_len)
+{
+    char color = 0;
+
+    for (int i = 0; i < nb_len; i++)
+        color = color * 10 + (str[pos + i] - '0');
+    write(1, &color, 1);
 }
 
 int compress_img(char *str, int len)
 {
     int i = 0;
-    char *tmp;
-    int int_tmp;
-    int j;
+    int nb_len = 0;
 
     if (len == 0)
         return (0);
     skip_header(str, &i);
     for (; i < len; i++) {
-        for (j = i; str[j] != '\n' && str[j] != '\0'; j++);
-        tmp = malloc(sizeof(char) * (j - i + 1));
-        my_strncpy_pos(tmp, str, j - i, i);
-        int_tmp = my_getnbr(tmp);
-        write(1, &int_tmp, 1);
-        free(tmp);
-        i = j;
+        for (nb_len = 0; str[i + nb_len] && str[i + nb_len] != '\n'; nb_len++);
+        print_color(str, i, nb_len);
+        i += nb_len;
     }
     return (0);
 }
