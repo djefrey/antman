bbd8db21c2e40231577c4fced721182faffedae1
rosenkranz <paul.rosenkranz@epitech.eu>
2021-01-23 13:44:55 +0100

    add .ppm compress

---
+++ antman/image.c
@@ -0,0 +1,69 @@
+/*
+** EPITECH PROJECT, 2021
+** antman-lzss [WSL: Ubuntu]
+** File description:
+** image
+*/
+
+int my_getnbr(char const *str)
+{
+    int i = 0;
+    int neg = 1;
+    long number = 0;
+
+    while (str[i] != '\0' && (str[i] == '-' || str[i] == '+')) {
+        if (str[i] == '-')
+            neg *= -1;
+        i++;
+    }
+    while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9')) {
+        number = number * 10 + (str[i] - '0');
+        i++;
+    }
+    number *= neg;
+    if (number > 2147483647 || number < -2147483648)
+        return (0);
+    return (number);
+}
+
+char *my_strncpy_pos(char *dest, char const *src, int n, int pos)
+{
+    unsigned char add_border = 1;
+    int i = 0;
+
+    while (i < n) {
+        dest[i] = src[i + pos];
+        if (src[i + pos] == '\0') {
+            add_border = 1;
+            break;
+        }
+        i++;
+    }
+    if (add_border)
+        dest[i] = '\0';
+    return (dest);
+}
+
+int compress_img(char *str, int len)
+{
+    int i = 0;
+    char *tmp;
+    int int_tmp;
+    int j;
+
+    for (int n = 0 ; n != 5; i++) {
+        if (str[i] == '\n')
+        n++;
+        my_putchar(str[i]);
+    }
+    for (; str[i]; i++) {
+        for (j = i ;str[j] != '\n' && str[j] != '\0'; j++);
+        tmp = malloc(sizeof(char) * (j - i + 1));
+        tmp = my_strncpy_pos(tmp, str, j - i, i);
+        int_tmp = my_getnbr(tmp);
+        write(1, &int_tmp, 1);
+        free(tmp);
+        i = j;
+        write(1, "\n", 1);
+    }
+}
--- include/antman.h
+++ include/antman.h
@@ -10,5 +10,8 @@
 
 int lzss(char *str, int len);
 int html(char *str, int len);
+int my_getnbr(char const *str);
+char *my_strncpy_pos(char *dest, char const *src, int n, int pos);
+int compress_img(char *str, int len);
 
 #endif /* !ANTMAN_H_ */
