ff6e231777e447b6340727e1e244fbd010af8d65
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2021-01-23 11:38:42 +0100

    files : free if read fails

--- common/files.c
+++ common/files.c
@@ -15,17 +15,20 @@ int read_file(char *path, char **str, int *len)
 {
     int fd = open(path, O_RDONLY);
     struct stat statbuf;
+    char *tmp;
 
     if (fd == -1 || stat(path, &statbuf) == 1)
         return (1);
     *len = statbuf.st_size;
-    *str = malloc(sizeof(char) * (*len + 1));
+    tmp = malloc(sizeof(char) * (*len + 1));
+    *str = tmp;
     if (!str) {
         close(fd);
         return (1);
     }
     if (read(fd, *str, *len) == -1) {
         close(fd);
+        free(tmp);
         return (84);
     }
     close(fd);
